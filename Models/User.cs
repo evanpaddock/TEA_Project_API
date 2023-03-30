48f386dcc1c9efa6f2a6e22890e4cafe0c32373f
Evan <112584442+evanpaddock@users.noreply.github.com>
2023-03-30 16:05:13 -0500

    user model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace TEA_Project_API.Models
+{
+    public class User
+    {
+        public int id{get; set;}
+        public string email{get; set;}
+        public string password{get; set;}
+        public string userName{get; set;}
+    }
+}
