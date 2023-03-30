b4d6d2805043968353811af3c6de142f3f71cfd8
evanpaddock <edpaddock@crimson.ua.edu>
2023-03-29 20:26:00 -0500

    commit

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -4,7 +4,7 @@ namespace TEA_Project_API
     {
         string Server = @"hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         string Username = "euxzcg6yn0uw8i7w";
-        string Password = "u69j7flom56a0msx";
+        string Password = "u5rktv5agvafabe3";
         string Port = "3306";
         string Database = "jspzik5rdmwk62p2";
         public string cs{get; set;}
