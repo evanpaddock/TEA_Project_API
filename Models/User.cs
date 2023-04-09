8fc4fbcd71111fc84e188ab6a2ad37952bb257a4
evanpaddock <edpaddock@crimson.ua.edu>
2023-04-09 17:35:15 -0500

    User Field Update

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,10 @@ namespace TEA_Project_API.Models
         public string UserName{get; set;}
         public string Password{get; set;}
         public string UserEmail{get; set;}
+        public string FirstName{get; set;}
+        public string LastName{get; set;}
         public DateOnly DateJoined{get; set;}
+        public string State{get; set;}
         public int Role_ID{get; set;}
     }
 }
