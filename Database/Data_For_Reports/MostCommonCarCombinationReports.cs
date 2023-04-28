dde39b83356d8a9e574e7521b37d085248970156
evanpaddock <edpaddock@crimson.ua.edu>
2023-04-28 16:34:09 -0500

    api update

--- Database/Data_For_Reports/MostCommonCarCombinationReports.cs
+++ Database/Data_For_Reports/MostCommonCarCombinationReports.cs
@@ -12,13 +12,9 @@ namespace TEA_Project_API.Database.Data_For_Reports
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = @"SELECT c2.Make, c1.Make, COUNT(DISTINCT c.Report_ID) as TimesCounted
-                            FROM cars_for_reports c JOIN cars_for_reports cr on (c.Report_ID = cr.Report_ID)
-		                        JOIN car c2 on (c.CarID = c2.CarID)
-		                        JOIN car c1 on (cr.CarID = c1.CarID)
-                            WHERE c.CarID != cr.CarID
-                            ORDER BY TimesCounted DESC
-                            LIMIT 10;";
+            string stm = @"SELECT Fuel_Type, COUNT(Fuel_Type) as TotalCount
+                            FROM car
+                            GROUP BY Fuel_Type;";
             using var cmd = new MySqlCommand(stm, con);
             using MySqlDataReader rdr = cmd.ExecuteReader();
 
@@ -26,7 +22,7 @@ namespace TEA_Project_API.Database.Data_For_Reports
 
             while (rdr.Read()) {
                 CarCombinations myCarCombination = new CarCombinations(){
-                    BothCarMakes=rdr.GetString(0) + " & " + rdr.GetString(1), TotalSame=rdr.GetInt32(2)
+                    FuelType=rdr.GetString(0), TotalSame=rdr.GetInt32(1)
                 };
                 
                 myCarCombinations.Add(myCarCombination);
